bccdbe46f88852ad04a6512544779d1ab512b163
Olga Golovkina <golovkinaos@bimacad.ru>
2020-12-03 14:46:07 +0300

    фикс 2

--- TestOne/Program.cs
+++ TestOne/Program.cs
@@ -7,7 +7,8 @@ namespace TestOne
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("TestOne");
+            Console.WriteLine("123");
+
         }
     }
 }
